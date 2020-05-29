1c4ae0a4fe7627c7bd2d2064dc04209220fe280f
Mitchell Perilstein <mitchell.perilstein@gmail.com>
2020-05-28 23:05:09 -0400

    Update readme with improved vision.

--- README.md
+++ README.md
@@ -1,26 +1,71 @@
-# Emacs Elisp Interface to IPFS
+# Emacs IPFS Project
 
-## Retrieve objects
-This package allows Emacs to retrieve IPFS and IPNS objects over the
-[HTTP API protocol](https://docs.ipfs.io/reference/api/http) or from
-[IPFS HTTP Gateways](https://ipfs.github.io/public-gateway-checker/).
+This project will create an Elisp package `ipfs-el` to access IPFS nodes, both with the [IPFS API
+protocol](https://docs.ipfs.io/reference/api/http) as well as [retrieve-only HTTP
+gateways](https://ipfs.github.io/public-gateway-checker/) for convenience. `ipfs-el` will also aim to
+provide some integration for `ipfs://` and `ipns://` paths and others such as magic file names.
 
-You can either run your own daemon locally or use a public gateway. The code will try local first.
+After that foundation, some applications will be explored such as `org-mode` IPFS links, an IPFS backend for
+the `package` package manager, and structured programming.
+
+## IPFS API Binding
 
 If you're running [your own daemon locally](https://github.com/ipfs/go-ipfs) which is also easy to do now
-[with a Docker container](https://hub.docker.com/r/ipfs/go-ipfs), it's assumed to be http://127.0.0.1:5001.
+[with a Docker container](https://hub.docker.com/r/ipfs/go-ipfs), it's assumed to be
+http://127.0.0.1:5001. Configure via variable `ipfs-api-url`.
+
+TBW: `add` and `cat`. 
+
+TBD: To be most useful, file, name and key management will also be needed.
+
+Following existing `url` package access patterns, synchronous and asynchronous versions will be given.
+
+See also [implementation doc](https://github.com/ipfs/go-ipfs/blob/master/docs/implement-api-bindings.md).
+
+## Retrieve Via Gateway
+
+See variable `ipfs-public-gateway`.
+
+If a node API is not available, we can still offer synchronous and asynchronous `get` via public IPFS HTTP
+gateways.  Local node daemons also offer a gateway interface but there's no point locally except testing.
+
+TBD: Automatic fallback to gateway on API `get/cat` failure, vs explicit calls to distinguish.
 
 ## Publish Objects
 
-to be written
+This is a little complicated because saving any change to a file implies a new CID, which is returned. 
+
+TBD: An IPFS name publish would make sense, eg, for a document, which will imply key management decisions.
+
+# Applications to Explore
 
 ## Package Manager
 
-This is also an IPFS ELPA/MELPA package implementation. Package managers are a
-current focus of the IPFS team, which it's especially well suited for.
+This is also an IPFS ELPA/MELPA
+[package implementation](https://www.gnu.org/software/emacs/manual/html_node/elisp/Package-Archives.html#Package-Archives).
+Package managers are a current focus of the IPFS team, which it's especially well suited for.
 This will be the initial focus of this project.
 
+## Org mode link handling
+
+Links in org mode are subject to edit, rot, etc. Supporting an ipfs://
+link type may be the killer app allowing whole org trees to be
+retrieved with graph-sync, or just referred to.
+
+## Structured Programming
+
+A personal interest of mine. Immutable, composable, content addressable chunks of code can be assembled into
+larger structures. In case you write the exact same normalized code as someone else, it will have the same
+CID and be de-duped. Explore in Elisp.
+
 # Roadmap
+
+`ipfs-el` Emacs packackage - ideally provide only a package, defadvice and hooks, no patching of existing packages
+
+- [ ] Add an ipns:// and ipfs:// URL scheme for retrieving
+- [ ] Investigate what web3:// would mean
+- [ ] `package-list`: resolve IPNS name and pull archive-contents from IPFS
+- [ ] `package-install`: pull selected packages from IPFS
 
 Package Index Publishing
 
@@ -33,10 +78,7 @@ Package Publishing
 - [ ] Fork https://github.com/melpa/package-build to pull packages git and publish each to IPFS
 - [ ] Augment the archive-contents structure to hold IPFS CIDs to each package
 
-Emacs packackage - ideally provide only a package, defadvice and hooks, no patching of existing packages
 
-- [ ] Add an ipns:// and ipfs:// URL scheme for retrieving
-- [ ] Investigate what web3:// would mean
-- [ ] `package-list`: resolve IPNS name and pull archive-contents from IPFS
-- [ ] `package-install`: pull selected packages from IPFS
+-----
 
+*Sic itur ad astra*
--- ipfs.el
+++ ipfs.el
@@ -24,41 +24,27 @@
 (defconst ipfs-boundary "---2a8ae6ad-f4ad-4d9a-a92c-6d217011fe0f---")
 
 (defun ipfs-fetch-upstream ()
+  "Retreive upstream archive-contents and return it as a list."
   (with-current-buffer
-      (url-https
-       (url-generic-parse-url ipfs-upstream-url)
-       (lambda (x)
-         (goto-char (point-min))
-         (re-search-forward "\n\n")
-         (let ((tmp (read (current-buffer))))
-           (if (eq 1 (car tmp))
-               (progn 
-                 (message "Retrieved %s packages from %s" (length tmp) ipfs-upstream-url)
-                 (setq ipfs-archive-contents tmp))
-             (error "Bad read from %s" ipfs-upstream-url))))
-       nil))
-  t)
-(ipfs-fetch-upstream)
-
-
-; ipfs-archive-contents 
-
-
-
-; (mapc 'kill-buffer (seq-filter (lambda (x) (string-match-p "melpa" (buffer-name x)))
-;                                (buffer-list)))
-
+    (url-retrieve-synchronously ipfs-upstream-url)
+    (goto-char (point-min))
+    (re-search-forward "^$")
+    (let ((fetched (read (current-buffer))))
+      (kill-buffer (current-buffer))
+      (if (and fetched (listp fetched) (eq 1 (car fetched)))
+          (progn 
+            (message "Retrieved %s packages from %s" (length (cdr fetched)) ipfs-upstream-url)
+            fetched)
+        (error "Bad read from %s" ipfs-upstream-url)))))
 
 ;; works 
-;; (ipfs-fetch-upstream)
-;; (mapcar 'car ipfs-archive-contents)
+;; (mapcar 'car (car (ipfs-fetch-upstream)))
 
-
-
-; MAYBE
+; ---- NO ----
 ; (custom-set-variables '(url-handler-regexp
 ;                        "\\`\\(https?\\|ipfs\\|ipns\\|ftp\\|file\\|nfs\\|ssh\\|scp\\|rsync\\|telnet\\)://"))
 
+; ---- YES ----
 ; (url-handler-mode 1)
 ; (find-file "https://example.com")
 
@@ -72,7 +58,44 @@
 ;; (url-generic-parse-url  "http://127.0.0.1:5001/")
 ;; #s(url "http" nil nil "127.0.0.1" 5001 "/" nil nil t nil t t)
 
-url-file-handlers
+(defconst ipfs-handler-regexp "\\(ipfs\\|ipns\\)://"
+  "IPFS and IPNS protocol URL regexp")
+
+(defun ipfs-file-handler (operation &rest args)
+    "Function called from the `file-name-handler-alist' routines.
+OPERATION is what needs to be done (`file-exists-p', etc).  ARGS are
+the arguments that would have been passed to OPERATION."
+    (message "ipfs-file-handler got (%s %s)" operation args)
+    (cond ((eq operation 'insert-file-contents) (error "XX"))
+                       ;; Handle any operation we don't know about.
+          (t (let ((inhibit-file-name-handlers
+                    (cons 'ipfs-file-handler
+                          (and (eq inhibit-file-name-operation operation)
+                               inhibit-file-name-handlers)))
+                   (inhibit-file-name-operation operation))
+               (apply operation args)))))
+ipfs-file-handler
+
+
+
+(url-handler-mode 1)
+t
+
+(find-file "ipfs://a/b")
+
+(file-exists-p "ipfs://a/b")
+
+(file-attributes "ipfs://foo")
+     (funcall #'ipfs-file-handler 'file-exists-p "xxx")
+ 
+(access-file "ipfs://xxtmp" "x")
+
+
+package-archives
+
+
+(setq file-name-handler-alist
+'(("\\`\\(https?\\|ftp\\|file\\|nfs\\|ssh\\|scp\\|rsync\\|telnet\\)://" . url-file-handler) ("\\`ipfs://" . ipfs-file-handler) ("\\(?:\\.tzst\\|\\.zst\\|\\.dz\\|\\.txz\\|\\.xz\\|\\.lzma\\|\\.lz\\|\\.g?z\\|\\.\\(?:tgz\\|svgz\\|sifz\\)\\|\\.tbz2?\\|\\.bz2\\|\\.Z\\)\\(?:~\\|\\.~[-[:alnum:]:#@^._]+\\(?:~[[:digit:]]+\\)?~\\)?\\'" . jka-compr-handler) ("\\.gpg\\(~\\|\\.~[0-9]+~\\)?\\'" . epa-file-handler) ("\\`/\\(\\([^/|:]+:[^/|:]*|\\)*[^/|:]+\\(:[^/|:]*\\)?\\)?\\'" . tramp-completion-file-name-handler) ("^/\\(\\(?:\\([a-zA-Z0-9-]+\\):\\(?:\\([^/|: 	]+\\)@\\)?\\(\\(?:[a-zA-Z0-9_.%-]+\\|\\[\\(?:\\(?:\\(?:[a-zA-Z0-9]+\\)?:\\)+[a-zA-Z0-9.]+\\)?]\\)\\(?:#[0-9]+\\)?\\)?|\\)+\\)?\\([a-zA-Z0-9-]+\\):\\(?:\\([^/|: 	]+\\)@\\)?\\(\\(?:[a-zA-Z0-9_.%-]+\\|\\[\\(?:\\(?:\\(?:[a-zA-Z0-9]+\\)?:\\)+[a-zA-Z0-9.]+\\)?]\\)\\(?:#[0-9]+\\)?\\)?:\\(.*$\\)" . tramp-file-name-handler) ("\\`/:" . file-name-non-special)))
 
 
 (defun my-switch-to-url-buffer (status)
@@ -101,8 +124,51 @@ url-file-handlers
     (with-current-buffer (url-retrieve-synchronously url)
       (read-from-string (cadr (split-string (buffer-string) "\n\n"))))))
 
-; ok (ipfs-get-api "/api/v0/cat?arg=QmZULkCELmmk5XNfCgTnCyFgAVxBRBXyDHGGMVoLFLiXEN")
-http://example.com
+; ok  (ipfs-get-api "/api/v0/cat?arg=QmZULkCELmmk5XNfCgTnCyFgAVxBRBXyDHGGMVoLFLiXEN"))
+
+(unwind-protect
+    (url-retrieve "http://127.0.0.1:5088/")
+    (url-retrieve "http://google.com" (lambda (status ) (message "callback %s" status)))
+#<buffer  *http google.com:80*-487123>
+
+(catch 'x (url-retrieve-synchronously "foo"))
+(unwind-protect (error "foo") (setq xx "oopsie"))
+(condition-case 666
+    (error nil)
+
+(setq baz 3)
+
+     (condition-case err
+         (error "Rats!")
+       ;; This is the handler; it is not a form.
+       (error (princ (format "The error was: %s" err))
+              2))
+The error was: (error Rats!)2
+
+
+
+
+
+(set-variable debug-on-error nil)
+
+Debugger entered--Lisp error: (file-error "make client process failed" "Connection refused" :name "127.0.0.1" :buffer #<buffer  *url-http-temp*> :host "127.0.0.1" :service 5088 :nowait nil :tls-parameters nil)
+  make-network-process(:name "127.0.0.1" :buffer #<buffer  *url-http-temp*> :host "127.0.0.1" :service 5088 :nowait nil :tls-parameters nil)
+  open-network-stream("127.0.0.1" #<buffer  *url-http-temp*> "127.0.0.1" 5088 :type plain :nowait nil)
+  url-open-stream("127.0.0.1" #<buffer  *url-http-temp*> "127.0.0.1" 5088 nil)
+  url-http-find-free-connection("127.0.0.1" 5088 nil)
+  url-http(#s(url :type "http" :user nil :password nil :host "127.0.0.1" :portspec 5088 :filename "//api/v0/cat?arg=QmZULkCELmmk5XNfCgTnCyFgAVxBRBXyDHGGMVoLFLiXEN" :target nil :attributes nil :fullness t :silent nil :use-cookies t :asynchronous nil) #f(compiled-function (&rest ignored) #<bytecode 0x304572d>) (nil))
+  url-retrieve-internal("http://127.0.0.1:5088//api/v0/cat?arg=QmZULkCELmmk5XNfCgTnCyFgAVxBRBXyDHGGMVoLFLiXEN" #f(compiled-function (&rest ignored) #<bytecode 0x304572d>) (nil) nil nil)
+  url-retrieve("http://127.0.0.1:5088//api/v0/cat?arg=QmZULkCELmmk5XNfCgTnCyFgAVxBRBXyDHGGMVoLFLiXEN" #f(compiled-function (&rest ignored) #<bytecode 0x304572d>) nil nil nil)
+  url-retrieve-synchronously("http://127.0.0.1:5088//api/v0/cat?arg=QmZULkCELmmk5XNfCgTnCyFgAVxBRBXyDHGGMVoLFLiXEN")
+  (set-buffer (url-retrieve-synchronously url))
+  (save-current-buffer (set-buffer (url-retrieve-synchronously url)) (car (cdr (split-string (buffer-string) "\n\n"))))
+  (let ((url-request-method "POST")) (save-current-buffer (set-buffer (url-retrieve-synchronously url)) (car (cdr (split-string (buffer-string) "\n\n")))))
+  ipfs-get-url("http://127.0.0.1:5088//api/v0/cat?arg=QmZULkCELmmk5XNfCgTnCyFgAVxBRBXyDHGGMVoLFLiXEN")
+  ipfs-get-api("/api/v0/cat?arg=QmZULkCELmmk5XNfCgTnCyFgAVxBRBXyDHGGMVoLFLiXEN")
+  (let ((ipfs-daemon-url "http://127.0.0.1:5088/")) (ipfs-get-api "/api/v0/cat?arg=QmZULkCELmmk5XNfCgTnCyFgAVxBRBXyDHGGMVoLFLiXEN"))
+  eval((let ((ipfs-daemon-url "http://127.0.0.1:5088/")) (ipfs-get-api "/api/v0/cat?arg=QmZULkCELmmk5XNfCgTnCyFgAVxBRBXyDHGGMVoLFLiXEN")) nil)
+
+
 
 ; ok(ipfs-get-api "/api/v0/cat?arg=QmVeAzDB73nzB8aYrTtkrqQfPKxEr56jPfTVB21UsdNAMp")
 
